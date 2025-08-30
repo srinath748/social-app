5c16dc5e3148aecff9e70bf650a00201012ff128
srinath748 <msrinath748@gmail.com>
2025-08-31 02:15:58 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -70,6 +70,8 @@ app.use((err, req, res, next) => {
 // Start Server
 // ==============================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
